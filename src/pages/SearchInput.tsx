8169cf59af6f9f9c88242ff4b1f6169fee929c3f
Shreyansh <shreyanshnanavati@gmail.com>
2025-05-02 19:42:09 +0530

    wip: added debounced search function

--- src/pages/SearchInput.tsx
+++ src/pages/SearchInput.tsx
@@ -1,20 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react";
+
+const debouncedSearch = (fn, delay:number) => {
+  let timerId  = null
+  return function (...args:any[]) {
+    if(timerId !== null){
+      clearInterval(timerId)
+    }
+    timerId = setTimeout(() => {fn.apply(this, args);}, delay)
+
+  }
+  return fn;
+};
 
 export default function SearchInput() {
-  
-  const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const handleSubmit = (e) => { 
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log("Search: ", searchTerm)
-
+    console.log("Search: ", searchTerm);
   };
 
   const handleSearchTerm = (e) => {
-    setSearchTerm(e.target.value)
-  }
-
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <div>
@@ -22,14 +31,19 @@ export default function SearchInput() {
         <form onSubmit={handleSubmit}>
           <div className="flex items-center gap-2 bg-white shadow-md p-4">
             <label htmlFor="">Search:</label>
-            <input type="text" className="p-2 border rounded-2xl" value={searchTerm} onChange={handleSearchTerm}  />
+            <input
+              type="text"
+              className="p-2 border rounded-2xl"
+              value={searchTerm}
+              onChange={handleSearchTerm}
+            />
             <button className="p-2 bg-blue-600 rounded text-white">
               Submit
             </button>
           </div>
         </form>
-        <div className='p-4 bg-white h-32 shadow-md rounded'>
-          <h2 className='text-2xl'>Jokes</h2>
+        <div className="p-4 bg-white h-32 shadow-md rounded">
+          <h2 className="text-2xl">Jokes</h2>
           <hr />
           <p>{searchTerm}</p>
         </div>
